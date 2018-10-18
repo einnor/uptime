ac916f355717e28895e1b49afa594d30fdc20430
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-10-18 18:05:18 +0300

    Parsing requests

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var http  = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
+
+	// Get the URL and parse it
+	var parsedUrl = url.parse(req.url, true);
+
+	// Get the path
+	var path = parsedUrl.pathname;
+
+	// Trim slasshes i.e /foo/bar/ will be /foo/bar
+	var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	console.log(trimmedPath);
+
+	// Send a response
 	res.end('Hey\n');
 });
 
